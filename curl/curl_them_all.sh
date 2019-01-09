83e0408b7783fedd46b25700f3d244f6019706ba
i,sylar <hilsoville@gmail.com>
2019-01-09 17:06:15 +0300

    Update curl_them_all.sh

--- curl/curl_them_all.sh
+++ curl/curl_them_all.sh
@@ -10,7 +10,7 @@ do
  echo "================================================"
  echo "===  $(tput setaf 1)Curl output for: $url$(tput sgr0)  ==="
  echo "================================================" 
-  curl -i -s -D -o /dev/null -X OPTIONS "http://$url" -m 5 | head
+  curl -i -s -D -o /dev/null -X OPTIONS "https://$url" -m 5 | head
  echo "================================================"
  echo -e '\n'
 done
